7e38ce66691e28f588cf570b7301810c525b0523
Lena Felica <leonatdg@mail.ru>
2023-06-12 11:20:13 +0300

    search in a BST

--- 700-searsch_in_a_BST.js
+++ 700-searsch_in_a_BST.js
@@ -4,9 +4,17 @@
 // Input: root = [4,2,7,1,3], val = 5
 // Output: []
 //
+
+//     2
+//    / \
+//   1   5
+//      /
+//     3
+//* return subnode (Null)
+//
 const searchBST = (root, val) => {
-   if(!root) return null;
-   if(root.val === val) return root;
+   if(!root) return null; // исключение
+   if(root.val === val) return root; // базовый случай дл выхода из рекурсии
 
    if(val > root.val) {
       return searchBST(root.right, val)
